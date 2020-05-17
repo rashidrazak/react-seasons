b6e03462ef942dafe2b944ba2c605e34e650320b
Rashid Razak <bib.nexus@gmail.com>
2020-05-18 02:59:34 +0800

    Use setState to update state object

--- src/index.js
+++ src/index.js
@@ -4,14 +4,35 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    // super call is compulsory
+    super(props)
+
+    // Initialize state
+    this.state = {
+      lat: null,
+      long: null
+    }
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        // Always use setState to update state object
+        this.setState({ 
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        })
+      },
       (error) => console.log(error)
     )
+  }
 
+  // render method is a must have in React class component
+  render() {
     return (
-      <div>Latitude: </div>
+      <div>
+        <div>Latitude: { this.state.lat }</div>
+        <div>Longitude: { this.state.long }</div>
+      </div>
     )
   }
 }
