147d11acd641f2bf953b1c8c700f2209ccba4e47
Rashid Razak <bib.nexus@gmail.com>
2020-05-18 02:11:49 +0800

    Detect user's geolocation

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (error) => console.log(error)
+  )
+  
   return (
     <SeasonDisplay />
   )
