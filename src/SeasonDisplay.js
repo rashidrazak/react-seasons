40800ee73404512e280456b88c17946509c789c6
Rashid Razak <bib.nexus@gmail.com>
2020-05-19 03:26:30 +0800

    Refactor code to use config object

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+// Best practice to have config object like this
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it\'s chilly',
+    iconName: 'snowflake'
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
-    return lat > 0 ? 'Summer' : 'Winter'
+    return lat > 0 ? 'summer' : 'winter'
   } else {
-    return lat > 0 ? 'Winter' : 'Summer'
+    return lat > 0 ? 'winter' : 'summer'
   }
 }
 
@@ -13,17 +25,18 @@ const SeasonDisplay = (props) => {
   const {lat, long} = props
 
   const season = getSeason(lat, new Date().getMonth())
-  const text = season === 'Winter' ? `Burr, it's chilly` : `Let's hit the beach`
-  const icon = season === 'Winter' ? 'snowflake' : 'sun'
+
+  // Get values from config object defined earlier
+  const {text, iconName} = seasonConfig[season]
 
   return (
     <div>
       {/* <div>Latitude: {lat}</div>
       <div>Longitude: {long}</div>
       <div>Season: {season}</div> */}
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   )
 }
