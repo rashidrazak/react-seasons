c0e323a06bfaabee642d34b3d87d137be889fec6
Rashid Razak <bib.nexus@gmail.com>
2020-05-19 03:09:23 +0800

    Add a helper function to determine season and use that function

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,13 +1,24 @@
 import React from 'react'
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'Summer' : 'Winter'
+  } else {
+    return lat > 0 ? 'Winter' : 'Summer'
+  }
+}
+
 const SeasonDisplay = (props) => {
   // Use object destructuring
   const {lat, long} = props
+
+  const season = getSeason(lat, new Date().getMonth())
 
   return (
     <div>
       <div>Latitude: {lat}</div>
       <div>Longitude: {long}</div>
+      <div>Season: {season}</div>
     </div>
   )
 }
